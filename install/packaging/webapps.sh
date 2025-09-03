d9de9bd78b7817b9f351398c1e1725875db54f84
emerson <emerson.argueta@springscs.org>
2025-09-03 08:00:02 -0700

    added missing webapps

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -6,10 +6,18 @@ omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr
 omarchy-webapp-install "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
 omarchy-webapp-install "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
 omarchy-webapp-install "Google Messages" https://messages.google.com/web/conversations https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png
+omarchy-webapp-install "Google Chat" https://chat.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-chat.png
+omarchy-webapp-install "Google Cloud" https://cloud.console.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-cloud-platform.png
+omarchy-webapp-install "Google Mail" https://mail.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gmail.png
+omarchy-webapp-install "Gemini" https://mail.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-gemini.png
 omarchy-webapp-install "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
 omarchy-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+omarchy-webapp-install "YouTube Music" https://music.youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube-music.png
 omarchy-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+omarchy-webapp-install "GitLab" https://gitlab.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gitlab.png
 omarchy-webapp-install "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
+omarchy-webapp-install "Sentry" https://sentry.io/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/sentry.png
+omarchy-webapp-install "Asana" https://app.asana.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/asana.png
 omarchy-webapp-install "Figma" https://figma.com/ https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
 omarchy-webapp-install "Discord" https://discord.com/channels/@me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
