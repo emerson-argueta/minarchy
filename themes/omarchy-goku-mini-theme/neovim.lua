21bd42531bbb21e8cdb52a014ac6d764d832f7a0
emerson <emerson.argueta@springscs.org>
2025-10-28 11:01:15 -0700

    update theme

--- themes/omarchy-goku-mini-theme/neovim.lua
+++ themes/omarchy-goku-mini-theme/neovim.lua
@@ -8,9 +8,9 @@ return {
             colors = {
                 -- Monotone shades (base00-base07)
                 base00 = "#ffffff", -- Default background
-                base01 = "#ece5e5", -- Lighter background (status bars)
+                base01 = "#957777", -- Lighter background (status bars)
                 base02 = "#ffffff", -- Selection background
-                base03 = "#ece5e5", -- Comments, invisibles
+                base03 = "#957777", -- Comments, invisibles
                 base04 = "#584b4b", -- Dark foreground
                 base05 = "#000000", -- Default foreground
                 base06 = "#000000", -- Light foreground
@@ -19,12 +19,12 @@ return {
                 -- Accent colors (base08-base0F)
                 base08 = "#a38b8b", -- Variables, errors, red
                 base09 = "#a38b8b", -- Integers, constants, orange
-                base0A = "#c2a6a6", -- Classes, types, yellow
+                base0A = "#745108", -- Classes, types, yellow
                 base0B = "#b49a9a", -- Strings, green
                 base0C = "#b69b9b", -- Support, regex, cyan
-                base0D = "#e7a95e", -- Functions, keywords, blue
+                base0D = "#bf8c4d", -- Functions, keywords, blue
                 base0E = "#a78e8e", -- Keywords, storage, magenta
-                base0F = "#c2a6a6", -- Deprecated, brown/yellow
+                base0F = "#926e38", -- Deprecated, brown/yellow
             },
         },
         config = function(_, opts)
